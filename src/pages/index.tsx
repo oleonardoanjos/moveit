2a3d57d6b35f7bde0e83d8a73cd2f9ae374a828d
Lass <leo_anjos1@hotmail.com>
2021-02-26 18:45:37 -0300

    Adicionando componente ChallengeBox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import Head from 'next/head';
+import { ChallengeBox } from "../components/ChallengeBox";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+
 import styles from '../styles/pages/Home.module.css';
-import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -21,7 +23,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
